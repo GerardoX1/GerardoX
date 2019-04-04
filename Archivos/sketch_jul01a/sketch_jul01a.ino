693a7565d3921674e096c8d0f0ce88d687cbdc31
GerardoX1 <yeral_way@hotmail.com>
2019-04-04 12:17:36 -0600

    Add files via upload

---
+++ Archivos/sketch_jul01a/sketch_jul01a.ino
@@ -0,0 +1,25 @@
+int dato1 = 0;
+int dato2 = 0;
+int datos[]={dato1,dato2};
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin (9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+dato1 = analogRead(0);
+dato2 = analogRead(1);
+
+datos[0]=dato1;
+datos[1]=dato2;
+
+Serial.print("A0");
+Serial.print(datos[0]);
+Serial.print("A1");
+Serial.println(datos[1]);
+
+
+}
---
+++ Archivos/sketch_jun26a/sketch_jun26a.ino
@@ -0,0 +1,11 @@
+int dato =0;
+
+void setup(){
+  Serial.begin (9600);
+}
+
+void loop(){
+  dato =analogRead(0);
+  Serial.println (dato);
+}
+
